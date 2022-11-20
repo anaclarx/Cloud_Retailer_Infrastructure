0cee3f0066d483ac8d958e6d5b31bf9978b958ef
Ana Clara Granato <anaclaragranato@gmail.com>
2022-11-20 23:59:45 +0100

    fixing consolidator convert string part

--- cloud-retailer-aws/src/retailer/Consolidator.java
+++ cloud-retailer-aws/src/retailer/Consolidator.java
@@ -82,52 +82,86 @@ public class Consolidator {
 
                 // read what we wrote
                 String dataString = new String(data, StandardCharsets.UTF_8);
-                System.out.println(dataString);
+               // System.out.println(dataString);
 
                 // manipulating the data
                 String[] tempArray = dataString.split("\\n");
                 String[][] dataArray = new String[tempArray.length - 1][6];
-                for (int i = 1; i < tempArray.length; i++){ //start in i = 1 not to get the header of the csv file
+                for (int i = 1; i < dataArray.length; i++){ //start in i = 1 not to get the header of the csv file
                     dataArray[i] = tempArray[i].split(",");
-
+                    
+                    //PRODUCTS PROFIT
+                    
+                    StringBuilder profitProduct = new StringBuilder(dataArray[i][3]);
+					profitProduct.deleteCharAt(0);
+					//System.out.println(dataArray[i][3]);
+					profitProduct.deleteCharAt(dataArray[i][3].length() - 2);
+					String profitFinal = profitProduct.toString();
+					
+					
+					//QUANTITY PER PRODUCT
+                    StringBuilder quantityProduct = new StringBuilder(dataArray[i][4]);
+					quantityProduct.deleteCharAt(0);
+					quantityProduct.deleteCharAt(dataArray[i][4].length() - 2);
+					String quantityFinal = quantityProduct.toString();
+					
+					//SOLD PER PRODUCT
+                    StringBuilder soldProduct = new StringBuilder(dataArray[i][5]);
+					soldProduct.deleteCharAt(0);
+					soldProduct.deleteCharAt(dataArray[i][5].length() - 2);
+					String soldFinal = soldProduct.toString();
+					
+					
+					
                     // profit per product
                     if (profit.get(dataArray[i][2]) != null){
-							
-                        profit.put(dataArray[i][2], Double.parseDouble(dataArray[i][3]) + profit.get(dataArray[i][2]));
+						
+                        profit.put(dataArray[i][2], Double.parseDouble( profitFinal) + profit.get(dataArray[i][2]));
                     } else {
-                        profit.put(dataArray[i][2], Double.parseDouble(dataArray[i][3]));
+                        profit.put(dataArray[i][2], Double.parseDouble(profitFinal));
                     }
 
                     // quantity per product
                     if (quantity.get(dataArray[i][2]) != null){
 							
-                        quantity.put(dataArray[i][2], Integer.parseInt(dataArray[i][4]) + quantity.get(dataArray[i][2]));
+                        quantity.put(dataArray[i][2], Integer.parseInt(quantityFinal) + quantity.get(dataArray[i][2]));
                     } else {
-                        quantity.put(dataArray[i][2], Integer.parseInt(dataArray[i][4]));
+                        quantity.put(dataArray[i][2], Integer.parseInt(quantityFinal));
                     }
 
                     // sold per product
                     if (sold.get(dataArray[i][2]) != null){
 							
-                        sold.put(dataArray[i][2], Double.parseDouble(dataArray[i][5]) + sold.get(dataArray[i][2]));
+                        sold.put(dataArray[i][2], Double.parseDouble(soldFinal) + sold.get(dataArray[i][2]));
                     } else {
-                        sold.put(dataArray[i][2], Double.parseDouble(dataArray[i][5]));
+                        sold.put(dataArray[i][2], Double.parseDouble(soldFinal));
                     }
-                }
+              }
                 
-                // Retailer's total profit
-                profitRetailer = profitRetailer + Double.parseDouble(dataArray[0][1]);
+                
+                
+				//PROFIT STORE
+                System.out.println(dataArray);
+                System.out.println(dataArray[1][1]);
+                StringBuilder profitStoreProduct = new StringBuilder(dataArray[1][1]);
+				profitStoreProduct.deleteCharAt(0);
+				profitStoreProduct.deleteCharAt(dataArray[1][1].length() - 2);
+				
+				String profitStoreFinal = profitStoreProduct.toString();
+				
+				// Retailer's total profit
+                profitRetailer = profitRetailer + Double.parseDouble(profitStoreFinal);
                 
                 // Most Profitable Store
-                if (Double.parseDouble(dataArray[0][1]) > biggerProfit){
-                    biggerProfit = Double.parseDouble(dataArray[0][1]);
-                    mostProfitableStore = dataArray[0][0];
+                if (Double.parseDouble(profitStoreFinal) > biggerProfit){
+                    biggerProfit = Double.parseDouble(profitStoreFinal);
+                    mostProfitableStore = dataArray[1][0];
                 }
 
                 // Least Profitable Store
-                if (Double.parseDouble(dataArray[0][1]) < smallestProfit){
-                    smallestProfit = Double.parseDouble(dataArray[0][1]);
-                    leastProfitableStore = dataArray[0][0];
+                if (Double.parseDouble(profitStoreFinal) < smallestProfit){
+                    smallestProfit = Double.parseDouble(profitStoreFinal);
+                    leastProfitableStore = dataArray[1][0];
                 }
                 
                 System.out.println("Total Retailer's Profit: " + profitRetailer);
